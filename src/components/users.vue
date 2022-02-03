902091cf90087ef8f49a48e3d4154b3ebdbace96
ÖZKAN SEMİZ <ozknsmz@hotmail.com>
2022-02-03 13:22:16 +0300

    added login and register forms

---
+++ src/components/users.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="user">
+    <h1>User list</h1>
+    <ul></ul>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data: () => ({
+      
+  }),
+};
+</script>
