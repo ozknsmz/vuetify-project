45e06d2d39f409e2923819d2f07524837be80416
ÖZKAN SEMİZ <ozknsmz@hotmail.com>
2022-02-06 23:00:45 +0300

    created some files route

--- router/index.js
+++ router/index.js
@@ -1,28 +1,46 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Dashboard from '../views/Dashboard.vue'
-import Users from '../views/Users.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Login from "../views/Login.vue";
+import Register from "../views/Register.vue";
+import Dashboard from "../views/Dashboard.vue";
+import User from "../views/Users.vue";
+import Home from "../views/Home.vue";
 
-
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'dashboard',
-    component: Dashboard
+    path: "/",
+    name: "home",
+    component: Home,
   },
-   {
-     path: '/users',
-     name: 'users',
-     component: Users
-   },
-]
+  {
+    path: "/login",
+    name: "login",
+    component: Login,
+  },
+  {
+    path: "/register",
+    name: "register",
+    component: Register,
+  },
+
+  {
+    path: "/admin/dashboard",
+    name: "dashboard",
+    component: Dashboard,
+  },
+  {
+    path: "/admin/users",
+    name: "users",
+    component: User,
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
