3048097a052d88f4cfd44022fb8aaebcaa83a3f9
ÖZKAN SEMİZ <ozknsmz@hotmail.com>
2022-02-06 23:00:32 +0300

    added components in main

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <Navbar />
+   <Admin/>
     <v-content class="ma-4">
       <router-view></router-view>
     </v-content>
@@ -8,12 +8,12 @@
 </template>
 
 <script>
-import Navbar from "@/components/Navbar";
+import Admin from '../src/components/Admin.vue'
 export default {
   name: "App",
 
   components: {
-    Navbar,
+    Admin,
   },
 
   data: () => ({
