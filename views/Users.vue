81745cb7fc41e644e2ae3c186e2480a8fa6fa4e7
ÖZKAN SEMİZ <ozknsmz@hotmail.com>
2022-02-06 19:41:49 +0300

    added users vue file

---
+++ views/Users.vue
@@ -0,0 +1,151 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="desserts"
+    :search="search"
+    sort-by="calories"
+    class="elevation-1"
+  >
+    <template v-slot:top>
+      <v-toolbar flat>
+        <v-toolbar-title> USERS </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Search"
+          single-line
+          hide-details
+        ></v-text-field>
+        <v-divider class="mx-4" inset vertical></v-divider>
+        <v-spacer></v-spacer>
+        <v-dialog v-model="dialog" max-width="500px"> </v-dialog>
+        <v-dialog v-model="dialogDelete" max-width="500px">
+          <v-card>
+            <v-card-title class="text-h5"
+              >Are you sure you want to delete this item?</v-card-title
+            >
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="closeDelete"
+                >Cancel</v-btn
+              >
+              <v-btn color="blue darken-1" text @click="deleteItemConfirm"
+                >OK</v-btn
+              >
+              <v-spacer></v-spacer>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-toolbar>
+    </template>
+    <template v-slot:item.actions="{ item }">
+      <v-icon small class="mr-2" @click="editItem(item)"> mdi-pencil </v-icon>
+      <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
+    </template>
+  </v-data-table>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    search: "",
+    dialog: false,
+    dialogDelete: false,
+    headers: [
+      {
+        text: "ID",
+        align: "start",
+        sortable: false,
+        value: "id",
+      },
+      { text: "Name", value: "name" },
+      { text: "Username", value: "username" },
+      { text: "Role", value: "role" },
+      { text: "Actions", value: "actions", sortable: false },
+    ],
+    desserts: [
+      {
+        id: 1,
+        name: "Oguzhan Armagan",
+        username: "ogzhnarmgn",
+        role: "ADMIN",
+      },
+      {
+        id: 2,
+        name: "Dogukan Biskiner",
+        username: "dgknbsknr",
+        role: "USER",
+      },
+      {
+        id: 3,
+        name: "Ahmet Soy",
+        username: "ahmtsy",
+        role: "USER",
+      },
+      {
+        id: 4,
+        name: "Özkan Semiz",
+        username: "ozknsmz",
+        role: "SUPERADMIN",
+      },
+    ],
+    editedIndex: -1,
+  }),
+
+  computed: {},
+
+  watch: {
+    dialog(val) {
+      val || this.close();
+    },
+    dialogDelete(val) {
+      val || this.closeDelete();
+    },
+  },
+
+  methods: {
+    editItem(item) {
+      this.editedIndex = this.desserts.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialog = true;
+    },
+
+    deleteItem(item) {
+      this.editedIndex = this.desserts.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialogDelete = true;
+    },
+
+    deleteItemConfirm() {
+      this.desserts.splice(this.editedIndex, 1);
+      this.closeDelete();
+    },
+
+    close() {
+      this.dialog = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
+    },
+
+    closeDelete() {
+      this.dialogDelete = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
+    },
+
+    save() {
+      if (this.editedIndex > -1) {
+        Object.assign(this.desserts[this.editedIndex], this.editedItem);
+      } else {
+        this.desserts.push(this.editedItem);
+      }
+      this.close();
+    },
+  },
+};
+</script>
